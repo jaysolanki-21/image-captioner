fbbc786db10178a076d3e8eb33225dcb58a8ce6d
Jay Solanki <jaysol8900@gmail.com>
2025-10-12 15:02:50 +0530

    backend setup

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -31,7 +31,7 @@ const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
 
@@ -62,7 +62,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
 
