237fcc97f070189e9b87c240c92494b7be983643
Jay Solanki <jaysol8900@gmail.com>
2025-11-17 08:08:51 +0530

    UPDATE caption generation rules for clarity and structure

--- backend/src/service/ai.service.js
+++ backend/src/service/ai.service.js
@@ -21,15 +21,27 @@ async function generateCaption(base64ImageFile, language = 'english') {
                 data: base64ImageFile,
             },
         },
-        { text: `Caption this image. Respond in ${targetLang}.` },
+        {
+          text: `
+You are a professional image captioning assistant.
+Write only one clean, natural caption for the given image.
+Rules:
+1. Respond in ${targetLang}.
+2. Do NOT use Markdown, bullet points, or any formatting characters like *, **, _, or quotes.
+3. Do NOT include multiple caption options.
+4. The caption must be concise, descriptive, and sound natural, suitable for social media.
+5. Output plain text only (no lists, no headings, no extra words).
+6. Caption should be between 50 to 150 words.
+          `,
+        }
     ];
 
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
         contents: contents,
-        config: {
-            
-        }
+        config: {   
+        },
+    
     });
 
     return response.text
